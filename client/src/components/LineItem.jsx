b82c9d3a78baac8b5bd52f23e44b71c2c2cc29bf
Graeme Folk <graemefolk@gmail.com>
2023-04-26 11:26:45 -0600

    initial layout of LineItem

--- client/src/components/LineItem.jsx
+++ client/src/components/LineItem.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import "./LineItem.css";
 
 export default function LineItem({ item }) {
-  return <div>{item.title}</div>;
+  return (
+    <div className="container">
+      <div className="child">
+        <img src={item.image} alt={item.title} />
+      </div>
+      <div className="child line-item__description">
+        <div className="item-title">
+          {item.swatchTitle.toUpperCase()} / {item.title} / {item.id}
+        </div>
+        <div className="item-swatch">
+          <div
+            className="swatch"
+            style={{ backgroundColor: item.swatchColor }}
+          ></div>
+          <div>{item.swatchTitle}</div>
+        </div>
+      </div>
+      <div className="child line-item__details">
+        ${item.price * item.quantity}
+      </div>
+    </div>
+  );
 }
