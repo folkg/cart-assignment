0e5f4271ce63e313e29139854832f35d58ace517
Graeme Folk <graemefolk@gmail.com>
2023-04-26 12:33:39 -0600

    create express route /api/item

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import itemRouter from "./routes/itemRoutes.js";
+
+const server = express();
+const hostname = "127.0.0.1";
+const port = 4000;
+
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/item", itemRouter);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
