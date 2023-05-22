12da1f64e44422bac8526165c784a5d39971bf74
Graeme Folk <graemefolk@gmail.com>
2023-05-22 14:47:42 -0600

    read and return JSON from file

--- rust_server/src/handlers.rs
+++ rust_server/src/handlers.rs
@@ -1,9 +1,17 @@
-use axum::Json;
+use std::fs;
 
-pub async fn item_handler() -> Json<&'static str> {
-    Json("Item works!")
+use axum::Json;
+use serde_json::Value;
+
+pub async fn item_handler() -> Json<Value> {
+    let data = fs::read_to_string("json/line_items.json").expect("Should be able to read file.");
+    let json = serde_json::from_str(&data).expect("Should have the correct JSON format.");
+    Json(json)
 }
 
-pub async fn delivery_handler() -> Json<&'static str> {
-    Json("Delivery works!")
+pub async fn delivery_handler() -> Json<Value> {
+    let data =
+        fs::read_to_string("json/delivery_dates.json").expect("Should be able to read file.");
+    let json = serde_json::from_str(&data).expect("Should have the correct JSON format.");
+    Json(json)
 }
