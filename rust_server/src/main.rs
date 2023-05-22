0c2dc77602b3a21d2d08faa2561f93666697492a
Graeme Folk <graemefolk@gmail.com>
2023-05-22 14:00:25 -0600

    initial Hello, World server

--- rust_server/src/main.rs
+++ rust_server/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    axum::Server::bind(&"127.0.0.1:4001".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .expect("server should not fail.")
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
