01122a7bb37f0ced799c70c4af7068b47013a28a
Graeme Folk <graemefolk@gmail.com>
2023-05-22 15:00:46 -0600

    retrieve query parameters in delivery_handler

--- rust_server/src/handlers.rs
+++ rust_server/src/handlers.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
-use axum::Json;
+use axum::{extract::Query, routing::post, Json};
+use serde::Deserialize;
 use serde_json::Value;
 
 pub async fn item_handler() -> Json<Value> {
@@ -9,9 +10,18 @@ pub async fn item_handler() -> Json<Value> {
     Json(json)
 }
 
-pub async fn delivery_handler() -> Json<Value> {
+#[derive(Deserialize)]
+pub struct DeliveryQueryParams {
+    postalCode: String,
+    lineItemId: String,
+}
+
+pub async fn delivery_handler(Query(params): Query<DeliveryQueryParams>) -> Json<Value> {
+    let postal_code = params.postalCode;
+    let line_item_id = params.lineItemId;
     let data =
         fs::read_to_string("json/delivery_dates.json").expect("Should be able to read file.");
     let json = serde_json::from_str(&data).expect("Should have the correct JSON format.");
+    println!("{} {}", postal_code, line_item_id);
     Json(json)
 }
