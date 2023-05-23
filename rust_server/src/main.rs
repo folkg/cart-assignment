f46f085f8b6c86b7f7898e9e960db8e5415cecbf
Graeme Folk <graemefolk@gmail.com>
2023-05-22 21:12:27 -0600

    add cors rule to allow any

--- rust_server/src/main.rs
+++ rust_server/src/main.rs
@@ -1,14 +1,18 @@
 use axum::{routing::get, Router};
+use tower_http::cors::{Any, CorsLayer};
 
 mod handlers;
 
 #[tokio::main]
 async fn main() {
+    let cors = CorsLayer::new().allow_origin(Any);
+
     let app = Router::new()
         .route("/api/item", get(handlers::item_handler))
-        .route("/api/delivery", get(handlers::delivery_handler));
+        .route("/api/delivery", get(handlers::delivery_handler))
+        .layer(cors);
 
-    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 4001));
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 4000));
     println!("Server running on http://{}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
