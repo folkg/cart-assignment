822e9d7d05b965d2813260b53cdd520064ca6b0f
Graeme Folk <graemefolk@gmail.com>
2023-05-23 14:59:04 -0600

    cleanup

--- node_server/routes/deliveryRoutes.js
+++ node_server/routes/deliveryRoutes.js
@@ -3,34 +3,6 @@ import { readFileSync } from "fs";
 
 const router = Router();
 
-// const DELIVERY_DATES = [
-//   {
-//     postal: "V",
-//     ids: [2],
-//     estimatedDeliveryDate: "Nov 24, 2021",
-//   },
-//   {
-//     postal: "V",
-//     ids: [1, 3],
-//     estimatedDeliveryDate: "Nov 19, 2021",
-//   },
-//   {
-//     postal: "M",
-//     ids: [2, 3],
-//     estimatedDeliveryDate: "Nov 22, 2021",
-//   },
-//   {
-//     postal: "M",
-//     ids: [1],
-//     estimatedDeliveryDate: "Dec 19, 2021",
-//   },
-//   {
-//     postal: "K",
-//     ids: [1, 2, 3],
-//     estimatedDeliveryDate: "Dec 24, 2021",
-//   },
-// ];
-
 router.get("/", (req, res) => {
   // will return the first found delivery date if multiple item ids match
   let delivery_dates;
