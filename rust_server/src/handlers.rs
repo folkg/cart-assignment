e4deee9261735f247c58028b436071c2d5771882
Graeme Folk <graemefolk@gmail.com>
2023-05-22 20:29:38 -0600

    create DeliverySate struct for JSON object

--- rust_server/src/handlers.rs
+++ rust_server/src/handlers.rs
@@ -1,27 +1,50 @@
 use std::fs;
 
-use axum::{extract::Query, routing::post, Json};
-use serde::Deserialize;
+use axum::{extract::Query, Json};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 pub async fn item_handler() -> Json<Value> {
+    // TODO: Better error handling to send back status 500 or something instead of panicing
     let data = fs::read_to_string("json/line_items.json").expect("Should be able to read file.");
-    let json = serde_json::from_str(&data).expect("Should have the correct JSON format.");
-    Json(json)
+    let items = serde_json::from_str(&data).expect("Should have the correct JSON format.");
+    Json(items)
 }
 
 #[derive(Deserialize)]
 pub struct DeliveryQueryParams {
-    postalCode: String,
-    lineItemId: String,
+    #[serde(rename = "postalCode")]
+    postal_code: String,
+
+    #[serde(rename = "lineItemId")]
+    line_item_id: String,
 }
 
-pub async fn delivery_handler(Query(params): Query<DeliveryQueryParams>) -> Json<Value> {
-    let postal_code = params.postalCode;
-    let line_item_id = params.lineItemId;
+#[derive(Deserialize, Serialize)]
+pub struct DeliveryDate {
+    #[serde(rename = "postal")]
+    postal_code: String,
+
+    #[serde(rename = "ids")]
+    item_ids: Vec<usize>,
+
+    #[serde(rename = "estimatedDeliveryDate")]
+    estimated_delivery_date: String,
+}
+
+pub async fn delivery_handler(params: Query<DeliveryQueryParams>) -> Json<Vec<DeliveryDate>> {
+    let DeliveryQueryParams {
+        postal_code,
+        line_item_id,
+        ..
+    } = params.0;
+
+    // TODO: Better error handling to send back status 500 or something instead of panicing
     let data =
         fs::read_to_string("json/delivery_dates.json").expect("Should be able to read file.");
-    let json = serde_json::from_str(&data).expect("Should have the correct JSON format.");
+    let delivery_dates: Vec<DeliveryDate> =
+        serde_json::from_str(&data).expect("Should have the correct JSON format.");
+
     println!("{} {}", postal_code, line_item_id);
-    Json(json)
+    Json(delivery_dates)
 }
